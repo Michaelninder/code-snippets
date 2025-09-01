a5d8bbbef27f47fa64f356f3d9774dfe1f102338
Michaelninder <fabianeternis@gmail.com>
2025-09-01 19:23:19 +0200

    feat: Finished getPrimeFactors.py.

--- Informatik/Py/getPrimeFactors.py
+++ Informatik/Py/getPrimeFactors.py
@@ -1,9 +1,39 @@
-num = input('Enter a number to get their PrimeFactors: ');
-primeList = getPrimeList(num); # getPrimeList() returns all PrimeNumbers taht are smaller than root(num) ...
-numPrimeFactors = [];
-PrimeNumberIndex = 0;
+import math
 
-def getPrimeFactors()
-    CurrentPrimeNumber = numPrimeFactors[PrimeNumberIndex]
-    while num > 1
-        if num %%
+num = int(input('Enter a number to get its Prime Factors: '))
+factors = getPrimeFactors(num)
+print(f"Prime factors of {num} are: {factors}")
+
+def getPrimeList(n):
+    # Erzeugt eine Liste aller Primzahlen kleiner oder gleich worzel(n).
+    primes = []
+    for num in range(2, int(math.isqrt(n)) + 1):
+        is_prime = True
+        for p in primes:
+            if num % p == 0:
+                is_prime = False
+                break
+        if is_prime:
+            primes.append(num)
+    return primes
+
+def getPrimeFactors(num):
+    # Gibt die Primfaktoren von num als Liste zurück.
+    primeList = getPrimeList(num)
+    numPrimeFactors = []
+    primeIndex = 0
+
+    while num > 1 and primeIndex < len(primeList):
+        currentPrime = primeList[primeIndex]
+        if num % currentPrime == 0:
+            numPrimeFactors.append(currentPrime)
+            num //= currentPrime
+        else:
+            primeIndex += 1
+
+    # Falls nach der Teilung noch ein Rest übrig ist, ist dieser selbst eine Primzahl
+    if num > 1:
+        numPrimeFactors.append(num)
+
+    return numPrimeFactors
+
