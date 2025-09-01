7d0e910b852bc6b5be29760644d2e40c928212b5
Michaelninder <fabianeternis@gmail.com>
2025-09-01 19:12:01 +0200

    some Layout-change and start of python-files (for school)

---
+++ Informatik/Py/getPrimeFactors.py
@@ -0,0 +1,9 @@
+num = input('Enter a number to get their PrimeFactors: ');
+primeList = getPrimeList(num); # getPrimeList() returns all PrimeNumbers taht are smaller than root(num) ...
+numPrimeFactors = [];
+PrimeNumberIndex = 0;
+
+def getPrimeFactors()
+    CurrentPrimeNumber = numPrimeFactors[PrimeNumberIndex]
+    while num > 1
+        if num %%
