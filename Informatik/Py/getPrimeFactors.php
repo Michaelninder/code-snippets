feecfb7bbf78ffe3fac62cb319650e086e6ca42c
Michaelninder <fabianeternis@gmail.com>
2025-09-13 10:04:41 +0200

    feat: create a Test-site

--- Informatik/Py/getPrimeFactors.php
+++ Informatik/Py/getPrimeFactors.php
@@ -1,8 +1,41 @@
 <?php
 
-function getPrimeFactrs(num): array {
-    $factrs = [];
-    for ($i = 0; $i < $num; $i++)
-        $factrs[$i] = 0;
-    return $factrs;
-    
+function primeFactors($num): array {
+
+    $primeList = [];
+    $limit = floor(sqrt($num));
+    for ($i = 2; $i <= $limit; $i++) {
+        $isPrime = true;
+        for ($j = 2; $j <= sqrt($i); $j++) {
+            if ($i % $j == 0) {
+                $isPrime = false;
+                break;
+            }
+        }
+        if ($isPrime) {
+            $primeList[] = $i;
+        }
+    }
+
+    $primeNumberIndex = 0;
+
+    while ($num > 1 && $primeNumberIndex < count($primeList)) {
+        $currentPrime = $primeList[$primeNumberIndex];
+        if ($num % $currentPrime == 0) {
+            $numPrimeFactors[] = $currentPrime;
+            $num = $num / $currentPrime;
+        } else {
+            $primeNumberIndex++;
+        }
+    }
+
+    if ($num > 1) {
+        $numPrimeFactors[] = $num;
+    }
+
+    return $numPrimeFactors;
+}
+
+$num = 84;
+$factors = primeFactors($num);
+echo "Primfaktoren von $num: " . implode(", ", $factors);
